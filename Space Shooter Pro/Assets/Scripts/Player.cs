384b3c794c8ed48fc225df91046549909d6e8754
george-mitchell <g.mitchell31@live.com>
2021-01-25 19:26:53 -0500

    Added Space Shooter

---
+++ Space Shooter Pro/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{   
+    [SerializeField]
+    private float _speed = 3.5f; // f suffix means float 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position = new position (0,0,0)
+        transform.position = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Recall, Time.deltatime converts frame speed to real speed
+        Vector3  direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime); 
+
+        // Set bounds for player by clamping vertical-motion and wrap the horizontal motion 
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4, 0), transform.position.z);
+
+        if (transform.position.x >= 11.2f)
+        {
+            transform.position = new Vector3(-11.2f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x <= -11.2f)
+        {
+            transform.position = new Vector3(11.2f, transform.position.y, transform.position.z);
+        }
+    }
+}
